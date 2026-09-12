e99b681d403d5b8accf7fad31695824c439e6f62
starfingar <deividsamueldsb@gmail.com>
2026-09-12 13:43:11 -0300

    concluir exercicio 09 sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+function sumAll(a, b) {
+  if (
+    typeof a !== 'number' || 
+    typeof b !== 'number' || 
+    !Number.isInteger(a) || 
+    !Number.isInteger(b) || 
+    a < 0 || 
+    b < 0
+  ) {
+    return 'ERROR';
+  }
 
-};
+  const min = Math.min(a, b);
+  const max = Math.max(a, b);
+  let sum = 0;
 
-// Do not edit below this line
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+
+  return sum;
+}
+
 module.exports = sumAll;
