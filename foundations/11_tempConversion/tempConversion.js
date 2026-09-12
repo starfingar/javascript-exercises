09d097510967127d1c39fd37c939498471ebf575
starfingar <deividsamueldsb@gmail.com>
2026-09-12 13:57:28 -0300

    concluir exercicio 11 tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) / 1.8;
+
+  return Number(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = celsius * 1.8 + 32;
+
+  return Number(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
