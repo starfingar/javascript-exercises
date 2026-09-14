559643ca21770913569b71961f12d498213ef99c
starfingar <deividsamueldsb@gmail.com>
2026-09-14 14:14:14 -0300

    concluir exercicio 13 palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,28 @@
-const palindromes = function () {
+const palindromes = function (texto) {
+  const textoMinusculo = texto.toLowerCase();
+  
+  const permitidos = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  
+  let textoLimpo = '';
+  for (let i = 0; i < textoMinusculo.length; i++) {
+    const caractere = textoMinusculo[i];
+    if (permitidos.includes(caractere)) {
+      textoLimpo += caractere;
+    }
+  }
 
+  let esquerda = 0;
+  let direita = textoLimpo.length - 1;
+
+  while (esquerda < direita) {
+    if (textoLimpo[esquerda] !== textoLimpo[direita]) {
+      return false;
+    }
+    esquerda++;
+    direita--;
+  }
+
+  return true;
 };
 
 // Do not edit below this line
