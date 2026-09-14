f62d26e9f9a8f050787f7a9e3f4acb087ad304af
starfingar <deividsamueldsb@gmail.com>
2026-09-14 11:24:13 -0300

    concluir exercicio 12 calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((acc, n) => acc + n, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((a, b) => a * b);
 };
 
-const power = function() {
-	
+const power = function(base, expoente) {
+	return Math.pow(base, expoente);
 };
 
-const factorial = function() {
-	
+const factorial = function(numero) {
+	if (numero === 0 || numero === 1) return 1;  
+  let resultado = 1;
+  for (let i = numero; i > 1; i--) {
+    resultado *= i;
+  }
+  
+  return resultado;
 };
 
 // Do not edit below this line
