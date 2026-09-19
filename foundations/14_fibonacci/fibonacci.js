62c1a34b1d6544b76d59efa44f337b32e3932a46
starfingar <deividsamueldsb@gmail.com>
2026-09-19 11:33:46 -0300

    concluir exercicio 14 fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(value) {
+  const number = parseInt(value, 10);
+  if (number < 0) {
+    return 'OOPS';
+  }
 
+  const sequencia = [0, 1];
+
+  for (let i = 2; i <= number; i++) {
+    let sum = sequencia[i - 1] + sequencia[i - 2];
+    sequencia.push(sum);
+  }
+
+  return sequencia[number];
 };
 
 // Do not edit below this line
