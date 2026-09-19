c1e54de154edf50ebb2d4e55e9a80b8ada2aab1c
starfingar <deividsamueldsb@gmail.com>
2026-09-19 12:02:34 -0300

    concluir exercicio 16 findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  return people.reduce((oldest, currentPerson) => {
+    const currentYear = new Date().getFullYear();
+    let currentAge;
+    
+    if (currentPerson.yearOfDeath) {
+      currentAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+    } else {
+      currentAge = currentYear - currentPerson.yearOfBirth;
+    }
+
+    let oldestAge;
+
+    if (oldest.yearOfDeath) {
+      oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+    } else {
+      oldestAge = currentYear - oldest.yearOfBirth;
+    }
+
+    if (currentAge > oldestAge) {
+      return currentPerson;
+    } else {
+      return oldest;
+    }
+}, people[0]);
 
 };
 
